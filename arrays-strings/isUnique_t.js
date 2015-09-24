8f3f25fba8093fb2557d9158ca5a1c2c70d62247
Nick Balestra <nick@balestra.ch>
2015-09-24 14:20:52 -0700

    isUnique

---
+++ arrays-strings/isUnique_t.js
@@ -0,0 +1,19 @@
+/*
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ */
+
+// test > determine if a string has all unique characters
+// # isUnique('nick') == true
+// # isUnique('balestra') == false
+ exports.isUnique = function(str){
+  var charHash = {};
+
+  return str.split('').reduce(function(prev, current){
+    if (prev && charHash[current] === undefined) {
+      charHash[current] = current;
+      return true;
+    }
+    return false;
+  }, true)
+};
